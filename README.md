e522f2b738ba0af94aab1cfd3dff846599c421b4
liujun <liujun@jianglaibao.cn>
2017-04-28 17:46:58 +0800

    initialize pine-mongoose project

---
+++ README.md
@@ -0,0 +1,2 @@
+# pine-mongoose
+mongoose middleware of [pine.js](https://www.npmjs.com/package/pine.js) framework, supported Mongodb data access.
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @namespace Pine
+ */
+
+/**
+ * @member {Application} Pine#Application
+ * @since 1.0.0
+ */
+module.exports = require('./lib/mongoose');
